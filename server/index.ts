d8b1678ee131134b79a763127adbedb884439b6f
voces <chakrawc@gmail.com>
2025-04-13 14:27:01 -0700

    fix: run build if not dev

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import { handleSocket } from "./client.ts";
 const isDev = Deno.args.includes("--dev");
 
 if (isDev) import("../scripts/dev.ts");
+else await (await import("../scripts/build.ts")).build();
 
 const dist = await Deno.realPath("dist");
 
